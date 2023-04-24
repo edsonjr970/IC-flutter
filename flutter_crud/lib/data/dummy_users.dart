93dea6ec73678c94342b6a365e6159005c6246d5
Edson Júnior <edson.juninho9797@gmail.com>
2023-04-23 21:44:42 -0300

    aulas_codelab

---
+++ flutter_crud/lib/data/dummy_users.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2022/08/15/15/38/animal-7388186_960_720.jpg'),
+  '2': const User(
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_960_720.jpg'),
+  '3': const User(
+      id: '3',
+      name: 'Alice',
+      email: 'alice@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2014/09/16/01/19/girl-447701_960_720.jpg'),
+  
+};
---
+++ flutter_crud2/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'hello_world.dart';
+
+void main() {
+  runApp(const MaterialApp(home: HelloWorld()));
+}
+
